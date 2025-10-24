4044bde6bc16f78a3a6c2ebcc6fedd68ad50ba00
rafapaezbas <rafaelpaezbastida@gmail.com>
2025-10-24 13:27:41 +0200

    platform_lock -> pear.lock

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const PLATFORM_URL = LOCALDEV
   : new URL('../../../', swapURL)
 
 const PLATFORM_DIR = toPath(PLATFORM_URL)
-const PLATFORM_LOCK = toPath(new URL('platform_lock', PLATFORM_URL))
+const PLATFORM_LOCK = toPath(new URL('pear.lock', PLATFORM_URL))
 
 const RUNTIME_EXEC = isWindows ? 'pear-runtime.exe' : 'pear-runtime'
 
